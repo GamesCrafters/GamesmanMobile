d17b5b72a8238491e31b39006cdc35d1126e8fb0
zipcodeman <zipcodeman@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-09-11 04:04:12 +0000

    Continued work on othello.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanMobile@5415 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/com/gamescrafters/othello/Othello.java
+++ src/com/gamescrafters/othello/Othello.java
@@ -1,9 +1,66 @@
 package com.gamescrafters.othello;
 
+import java.util.Stack;
+
+// import com.gamescrafters.connect4.GUIGameBoard;
 import com.gamescrafters.gamesmanmobile.GameActivity;
+import com.gamescrafters.gamesmanmobile.MoveValue;
+import com.gamescrafters.gamesmanmobile.R;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Othello extends GameActivity {
+	static final String GAME_NAME = "othello";
 
+	private TextView turnTextView, remoteTextView, gameOverTextView;
+	private ImageButton turnImage;
+	private Drawable bluePiece, redPiece;
+	// private CompPlays compPlaying = new CompPlays();
+
+	// Game g = null;
+	// GUIGameBoard gb;
+	MoveValue[] values = null;
+	String previousValue = "win";
+	int delay;
+	Stack<Integer> previousMoves, nextMoves; // Stacks of previousMoves and nextMoves, which is used to undo and redo moves.
+
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState); 
+		setGameView(R.layout.othello_game);
+		
+		initResources();
+		
+		setBoard(4,4);
+	}
+	
+	/**
+	 * Initializes the GUI elements used for the tiles, etc.
+	 */
+	private void initResources(){
+		Resources res = getResources();
+		bluePiece = res.getDrawable(R.drawable.oth_bluepiece);
+		redPiece = res.getDrawable(R.drawable.oth_redpiece);
+
+		turnTextView = (TextView) findViewById(R.id.oth_turn); 
+		turnImage = (ImageButton) findViewById(R.id.oth_turnImage);
+		remoteTextView = (TextView) findViewById(R.id.oth_remoteness);
+		gameOverTextView = (TextView) findViewById(R.id.oth_gameOver);	
+	}
+	
+	/**
+	 * Initializes the internal state and GUI of the Connect 4 board. 
+	 * @param width The width of the board to construct.
+	 * @param height The height of the board to construct.
+	 */
+	private void setBoard(int width, int height) { 
+		
+	}
+	
 	@Override
 	public void doMove(String move) {
 		// TODO Auto-generated method stub
--- src/com/gamescrafters/othello/OthelloOptions.java
+++ src/com/gamescrafters/othello/OthelloOptions.java
@@ -4,6 +4,7 @@ import com.gamescrafters.gamesmanmobile.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,5 +22,6 @@ public class OthelloOptions extends Activity{
 				OthelloOptions.this.startActivity(myIntent);
 			}
 		});
+
 	}
 }
