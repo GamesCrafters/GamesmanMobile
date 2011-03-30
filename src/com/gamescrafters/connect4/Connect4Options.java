880b4a46a7d71b3838b899ea115c081d4df2534d
G_Vardoyan <G_Vardoyan@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-03-30 19:38:42 +0000

    Made some changes to Connect4 Options
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanMobile@6073 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/com/gamescrafters/connect4/Connect4Options.java
+++ src/com/gamescrafters/connect4/Connect4Options.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.gamescrafters.gamesmanmobile.R;
 
@@ -18,6 +19,9 @@ public class Connect4Options extends Activity {
 	Spinner select_rows;
 	Spinner select_columns;
 	EditText select_time;
+	RadioButton comp1;
+	RadioButton comp2;
+	TextView time;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -39,14 +43,19 @@ public class Connect4Options extends Activity {
 		select_rows.setSelection(5); // default to 6 rows
 		
 		select_time = (EditText) findViewById(R.id.c4_select_time);
+		time = (TextView) findViewById(R.id.TextView01);
+		comp1 = (RadioButton) findViewById(R.id.c4_RadioButtonComputer1);
+		comp2 = (RadioButton) findViewById(R.id.c4_RadioButtonComputer2);
+		
+		changeVisibility(false);
 		
 		OnClickListener mylistener = new OnClickListener() {
 			public void onClick(View v) {
 				Intent myIntent = new Intent(Connect4Options.this, Connect4.class); // used to launch your new activity
 
 				// used to pass data to the Connect4 Activity class.
-				myIntent.putExtra("isPlayer1Computer", ((RadioButton) findViewById(R.id.c4_RadioButtonComputer1)).isChecked()); 
-				myIntent.putExtra("isPlayer2Computer", ((RadioButton) findViewById(R.id.c4_RadioButtonComputer2)).isChecked());
+				myIntent.putExtra("isPlayer1Computer", comp1.isChecked()); 
+				myIntent.putExtra("isPlayer2Computer", comp2.isChecked());
 				myIntent.putExtra("numRows", select_rows.getSelectedItemPosition()+1);
 				myIntent.putExtra("numCols", select_columns.getSelectedItemPosition()+1);
 				myIntent.putExtra("numDelay", select_time.getText().toString());
@@ -54,7 +63,40 @@ public class Connect4Options extends Activity {
 				Connect4Options.this.startActivity(myIntent);
 			}
 		};
+		
+		OnClickListener listener = new OnClickListener() {
+			public void onClick(View v) {
+				boolean c1, c2;
+				c1 = comp1.isChecked();
+				c2 = comp2.isChecked();
+				
+				if (c1 && c2) {
+					changeVisibility(true);
+				} else {
+					changeVisibility(false);
+				}
+			}
+		};
 
 		findViewById(R.id.c4_PlayConnect4Button).setOnClickListener(mylistener);
+		findViewById(R.id.c4_RadioButtonComputer1).setOnClickListener(listener);
+		findViewById(R.id.c4_RadioButtonComputer2).setOnClickListener(listener);
+		findViewById(R.id.c4_RadioButtonHuman1).setOnClickListener(listener);
+		findViewById(R.id.c4_RadioButtonHuman2).setOnClickListener(listener);
 	}
+	
+	private void changeVisibility(boolean v) {
+		int invis, vis;
+		invis = View.INVISIBLE;
+		vis = View.VISIBLE;
+		
+		if (v) {
+			time.setVisibility(vis);
+			select_time.setVisibility(vis);
+		} else {
+			time.setVisibility(invis);
+			select_time.setVisibility(invis);
+		}
+	}
+	
 }
