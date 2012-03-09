5b40fcb120bbce751c70c6702ac3737f258df7d3
sevenofdiamonds <sevenofdiamonds@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-03-09 02:24:21 +0000

    Game.java -- Added constructor, changeTurn(), getter and setter methods
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanMobile@6704 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/com/gamescrafters/abstractGame/Game.java
+++ src/com/gamescrafters/abstractGame/Game.java
@@ -5,13 +5,22 @@ public abstract class Game {
 	private Player player1;
 	private Player player2;
 	private boolean player1Turn;
-	// Move history? 
 	
-	// Will add in the arguments later
+	public Game(Board b, Player p1, Player p2){
+		myBoard = b;
+		player1 = p1;
+		player2 = p2;
+		player1Turn = true;
+	}
+	
 	public abstract void displayBoard();
 	
 	public boolean isPlayer1Turn() {
 		return player1Turn;
+	}
+	
+	public void changeTurn() {
+		player1Turn = !player1Turn;
 	}
 	
 	public abstract boolean isValidMove(Object location, Piece piece);
@@ -19,5 +28,37 @@ public abstract class Game {
 	public abstract void makeMove(Object location, Piece piece);
 	
 	public abstract int gameOver(); //Maybe return 1 for win, 0 for lose, 2 for draw, or something
+
+	// Automatically-generated getters/setters:
+	
+	public Board getMyBoard() {
+		return myBoard;
+	}
+
+	public void setMyBoard(Board myBoard) {
+		this.myBoard = myBoard;
+	}
+
+	public Player getPlayer1() {
+		return player1;
+	}
+
+	public void setPlayer1(Player player1) {
+		this.player1 = player1;
+	}
+
+	public Player getPlayer2() {
+		return player2;
+	}
+
+	public void setPlayer2(Player player2) {
+		this.player2 = player2;
+	}
+
+	public void setPlayer1Turn(boolean player1Turn) {
+		this.player1Turn = player1Turn;
+	}
+	
+	
 
 }
